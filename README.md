f281537317586bb5b1bad60285a34322bfbc8c79
Robert Coleman <github@robert.net.nz>
2013-12-07 23:31:51 +1300

    initial import

---
+++ README.md
@@ -0,0 +1,5 @@
+This is a [https://chocolatey.org](Chocolatey) package for [MingW-get](https://mingw.org).
+
+It'll select the download version 0.6.2 (beta-20131004), silently install and add the `mingw-get` binary to your PATH.
+
+I have no affiliation with MingW or Chocolatey. No warranties or rights implied.
---
+++ tools/chocolateyInstall.ps1
@@ -0,0 +1,36 @@
+﻿$package = 'mingw-get'
+
+try {
+  $files = @("mingw-get-0.6.2-mingw32-beta-20131004-1-bin.tar.xz",
+  "mingw-get-0.6.2-mingw32-beta-20131004-1-lic.tar.xz",
+  "mingw-get-setup-0.6.2-mingw32-beta-20131004-1-dll.tar.xz",
+  "mingw-get-setup-0.6.2-mingw32-beta-20131004-1-xml.tar.xz")
+
+  $downloadLocation = "http://prdownloads.sourceforge.net/mingw/"
+  $downloadSuffix = "?download"
+
+  $binRoot = Get-BinRoot
+  Write-Debug "Bin Root is $binRoot"
+  $installDir = Join-Path "$binRoot" 'MinGW'
+
+  if (![System.IO.Directory]::Exists($installDir)) {[System.IO.Directory]::CreateDirectory($installDir)}
+
+  $tempDir = "$env:TEMP\chocolatey\$($package)"
+  if (![System.IO.Directory]::Exists($tempDir)) {[System.IO.Directory]::CreateDirectory($tempDir)}
+
+  foreach ($file in $files) {
+    $url = -join ($downloadLocation, $file, $downloadSuffix)
+    $filePath = Join-Path $tempDir $file
+    Get-ChocolateyWebFile "$package" "$filePath" "$url"
+	Write-Host "Extracting `'$filePath`' to `'$installDir`'"
+	Start-Process "7za" -ArgumentList "x `"$filePath`" -so | 7za x -ttar -si -y -o`"$installDir`"" -Wait
+  }
+  Copy-Item "$installDir\var\lib\mingw-get\data\defaults.xml" "$installDir\var\lib\mingw-get\data\profile.xml"
+  Write-Host "Adding `'$installDir\bin`' to the path and the current shell path"
+  Install-ChocolateyPath "$installDir\bin" 'user'
+  $env:Path += ";$installDir\bin"
+  Write-ChocolateySuccess $package
+} catch {
+  Write-ChocolateyFailure $package "$($_.Exception.Message)"
+  throw
+}
