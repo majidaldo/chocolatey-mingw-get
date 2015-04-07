0a8d50e9103a4f2868d32c5894656d30d3848951
Dmitry Kireev <dmitry@kireev.co>
2015-04-06 20:51:19 -0700

    Add path & update environment

--- tools/chocolateyInstall.ps1
+++ tools/chocolateyInstall.ps1
@@ -2,5 +2,6 @@
 $url='http://hivelocity.dl.sourceforge.net/project/mingw/Installer/mingw-get/mingw-get-0.6.2-beta-20131004-1/mingw-get-0.6.2-mingw32-beta-20131004-1-bin.zip'
 $binRoot = Get-BinRoot
 $installDir = Join-Path "$binRoot" 'mingw64'
-
+Install-ChocolateyPath $installDir 'Machine'
 Install-ChocolateyZipPackage "$packageName" $url $installDir
+Update-SessionEnvironment
